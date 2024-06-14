33defb85310919951d998e2d0c78443b16affba4
gardogit <edgardo.irm@gmail.com>
2024-06-14 18:16:36 -0400

    Implementing Control Flow and Conditional Statements

---
+++ controlFlow/control_flow.js
@@ -0,0 +1,38 @@
+let userRole = "admin";
+let accessLevel;
+let isLoggedIn = true;
+let userMessage;
+let userType = "subscriber";
+let userCategory;
+
+if (isLoggedIn) {
+    switch (userType) {
+        case "admin":
+            userCategory = "Administrator";
+            break;
+        case "manager":
+            userCategory = "Manager";
+            break;
+        case "subscriber":
+            userCategory = "Subscriber";
+            break;
+        default:
+            userCategory = "Unknown";
+    }
+    if (userRole === "admin") {
+        userMessage = "Welcome, Admin!";
+        accessLevel = "Full access granted";
+    } else {
+        userMessage = "Welcome, User!";
+        if (userRole === "manager") {
+            accessLevel = "Limited access granted";
+        }
+    }
+} else {
+    userMessage = "Please log in to access the system.";
+    accessLevel = "No access granted";
+}
+
+console.log("Access Level:", accessLevel);
+console.log("User Message:", userMessage);
+console.log("User Category:", userCategory);
